43291ea0d981ca3704c5e992ed17636ca01ba512
jianglimin <82474481@qq.com>
2018-11-27 16:56:09 +0800

    1.01

---
+++ index.js
@@ -0,0 +1,31 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        list: [
+            {
+                id: 1,
+                name: "iphone",
+                price: 9999,
+                count:1
+            },
+            {
+                id: 2,
+                name: "ipad Pro",
+                price: 5888,
+                count:1
+            },
+            {
+                id: 3,
+                name: "Macbook Pro",
+                price: 21888,
+                count:1
+            }
+        ]
+    },
+    compute: {
+
+    },
+    methods: {
+
+    }
+})
