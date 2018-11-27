972918bcb0519fa45c8c98c85c9f7ff5ba4dd546
Mmmmmr <82474481@qq.com>
2018-11-27 22:07:16 +0800

    complete

--- index.js
+++ index.js
@@ -22,10 +22,28 @@ var app = new Vue({
             }
         ]
     },
-    compute: {
-
+    computed: {
+        totalPrice: function () {
+            var total = 0;
+            for (var i = 0; i < this.list.length; i++) {
+                var item = this.list[i];
+                total += item.price * item.count;
+            }
+            return total.toString();
+        }
     },
     methods: {
-
+        handleRuduce: function (index) {
+            if (this.list[index].count === 1) {
+                return;
+            }
+            this.list[index].count--;
+        },
+        handleAdd: function (index) {
+            this.list[index].count++;
+        },
+        handleRemove: function (index) {
+            this.list.splice(index, 1);
+        }
     }
 })
